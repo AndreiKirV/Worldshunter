b951ca311a8cb1815c4dc5aba8f9eef87d7cf63e
AndreOn <94354513+AndreiKirV@users.noreply.github.com>
2024-05-12 17:02:34 +0300

    Update CardMB.cs

--- Assets/Scripts/MBs/UI/CardMB.cs
+++ Assets/Scripts/MBs/UI/CardMB.cs
@@ -3,10 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class CardMB : MonoBehaviour
+public class CardMB : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public RectTransform RectTransform;
+    public Transform ParentTransform;
+    public Transform TemporaryParentTransform;
     public List<Element> _elements = new List<Element>()
     {
         new Element("Cost"),
@@ -19,6 +23,21 @@ public class CardMB : MonoBehaviour
         new Element("Special"),
         new Element("Portrait")
     };
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log($"Enter {this.name}");
+        transform.parent = TemporaryParentTransform;
+        RectTransform.sizeDelta = new Vector2(300, 400);
+        RectTransform.anchoredPosition = new Vector2(RectTransform.anchoredPosition.x, 0);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log($"Exit {this.name}");
+        RectTransform.sizeDelta = new Vector2(150, 200);
+        transform.parent = ParentTransform;
+    }
 
     [Serializable]
     public sealed class Element
